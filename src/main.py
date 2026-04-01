776faeda8e4d6e6e9e5bce912ea51fefc8cc6707
Сергій <serzal2005@gmail.com>
2026-04-01 21:48:02 +0300

    feat: додаємо функцію farewell()

---
+++ src/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def greet(name: str) -> str:
+    """Повертає вітальне повідомлення."""
+    return f"Привіт, {name}!"
+
+def farewell(name: str) -> str:
+    """Повертає прощальне повідомлення."""
+    return f"До побачення, {name}!"
+
+if __name__ == "__main__":
+    print(greet("Студент"))
+    print(farewell("Студент"))
